a5f9356ada4fb328062fe30496c40470216be806
Jack Copland <jack@jaxwax.com.au>
2020-04-20 12:34:44 +1000

    basic cil functionality finished

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use structopt::StructOpt;
+use std::{fs::File, io::prelude::*, io::BufReader, io::Write};
+use failure::ResultExt;
+use exitfailure::ExitFailure;
+#[derive(StructOpt, Debug)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
+fn main() -> Result<(), ExitFailure> {
+    let args = Cli::from_args();
+
+    let content = File::open(&args.path).with_context(|_| format!("Could not read the file {:?}", &args.path))?;
+    let reader = BufReader::new(content);
+
+    let stdout = std::io::stdout();
+    let mut handle = std::io::BufWriter::new(stdout);
+
+    for line in reader.lines() {
+        writeln!(handle, "{}", line.unwrap())?;
+    }
+    
+    handle.flush().unwrap();
+    
+    Ok(())
+
 }
